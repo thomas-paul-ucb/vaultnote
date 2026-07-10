8ca40f0f759f3478285d6ce770910525baf06687
Thomas Paul <610thomass@gmail.com>
2026-07-09 23:33:44 -0400

    Add Spring Boot project skeleton

---
+++ src/main/java/com/vaultnote/vaultnote/VaultnoteApplication.java
@@ -0,0 +1,13 @@
+package com.vaultnote.vaultnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaultnote/vaultnote/VaultnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaultnote.vaultnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
