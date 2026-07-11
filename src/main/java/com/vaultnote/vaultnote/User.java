dca31ab60b62f458c1b8cbc31cb707a76aedfd1f
Thomas Paul <610thomass@gmail.com>
2026-07-11 10:42:31 -0400

    Fix table name conflict (user is a reserved word in Postgres)

--- src/main/java/com/vaultnote/vaultnote/User.java
+++ src/main/java/com/vaultnote/vaultnote/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -15,7 +17,6 @@ public class User {
     private String username;
     private String password;
 
-    // Getters and setters
     public Long getId() {
         return id;
     }
